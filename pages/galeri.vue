360e4736f8f2ffcbe6efc5d4fdc63eb77ed54269
fauzanfirda <158239926+fauzanfirda@users.noreply.github.com>
2024-12-29 22:48:10 +0700

    Add files via upload

---
+++ pages/galeri.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="container-fluid">
+    <h1 class="mt-5 text-center">galeri</h1>
+    <div class="row p-5 m-5">
+    <div class="col-3 p-2"><img src="../assets/home.jpg" style="width: 100%;" alt=""></div>
+    <div class="col-3 p-2"><img src="../assets/home.jpg" style="width: 100%;" alt=""></div>
+    <div class="col-3 p-2"><img src="../assets/home.jpg" style="width: 100%;" alt=""></div>
+    <div class="col-3 p-2"><img src="../assets/home.jpg" style="width: 100%;" alt=""></div>
+    <div class="col-3 p-2"><img src="../assets/home.jpg" style="width: 100%;" alt=""></div>
+    <div class="col-3 p-2"><img src="../assets/home.jpg" style="width: 100%;" alt=""></div>
+    <div class="col-3 p-2"><img src="../assets/home.jpg" style="width: 100%;" alt=""></div>
+    <div class="col-3 p-2"><img src="../assets/home.jpg" style="width: 100%;" alt=""></div>
+    <div class="col-3 p-2"><img src="../assets/home.jpg" style="width: 100%;" alt=""></div>
+    <div class="col-3 p-2"><img src="../assets/home.jpg" style="width: 100%;" alt=""></div>
+    <div class="col-3 p-2"><img src="../assets/home.jpg" style="width: 100%;" alt=""></div>
+    <div class="col-3 p-2"><img src="../assets/home.jpg" style="width: 100%;" alt=""></div>
+    <div class="col-3 p-2"><img src="../assets/home.jpg" style="width: 100%;" alt=""></div>
+    <div class="col-3 p-2"><img src="../assets/home.jpg" style="width: 100%;" alt=""></div>
+    <div class="col-3 p-2"><img src="../assets/home.jpg" style="width: 100%;" alt=""></div>
+    <div class="col-3 p-2"><img src="../assets/home.jpg" style="width: 100%;" alt=""></div>
+    <div class="col-3 p-2"><img src="../assets/home.jpg" style="width: 100%;" alt=""></div>
+    <div class="col-3 p-2"><img src="../assets/home.jpg" style="width: 100%;" alt=""></div>
+    <div class="col-3 p-2"><img src="../assets/home.jpg" style="width: 100%;" alt=""></div>
+    <div class="col-3 p-2"><img src="../assets/home.jpg" style="width: 100%;" alt=""></div>
+    <div class="col-3 p-2"><img src="../assets/home.jpg" style="width: 100%;" alt=""></div>
+    <div class="col-3 p-2"><img src="../assets/home.jpg" style="width: 100%;" alt=""></div>
+    <div class="col-3 p-2"><img src="../assets/home.jpg" style="width: 100%;" alt=""></div>
+
+    </div>
+    </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,34 @@
+<template>
+    <img src="../assets/home.jpg" style="width: 100%;" alt="">
+    <div class="container-fluid">
+    <h1 class="text-center p-5">BERITA</h1>
+        <div class="row m-5">
+            <div class="col-3">
+                <div class="card p-2">
+                    <img src="../assets/home.jpg" style="width: 100%;" alt="">
+                    <h5 class="p-5">judul berita</h5>
+                </div>
+            </div>
+            <div class="col-3">
+                <div class="card p-2">
+                    <img src="../assets/home.jpg" style="width: 100%;" alt="">
+                    <h5 class="p-5">judul berita</h5>
+                </div>
+            </div>
+            <div class="col-3">
+                <div class="card p-2">
+                    <img src="../assets/home.jpg" style="width: 100%;" alt="">
+                    <h5 class="p-5">judul berita</h5>
+                </div>
+            </div>
+            <div class="col-3">
+                <div class="card p-2">
+                    <img src="../assets/home.jpg" style="width: 100%;" alt="">
+                    <h5 class="p-5">judul berita</h5>
+                </div>
+            </div>
+          
+
+        </div>
+    </div>
+</template>
